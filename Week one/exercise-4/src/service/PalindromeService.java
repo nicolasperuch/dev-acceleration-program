33240a1d7a88119c909656dad6c4bcda37bab901
Maíra Oliveira <mairasetecores@gmail.com>
2017-11-13 15:16:45 -0200

    creating method isPalindrome

--- Week one/exercise-4/src/service/PalindromeService.java
+++ Week one/exercise-4/src/service/PalindromeService.java
@@ -1,4 +1,21 @@
 package service;
 
 public class PalindromeService {
+
+    public static boolean isPalindrome(int nr) {
+
+        int reversedNumber = 0;
+
+        int value = nr;
+
+        while (value > 0) {
+
+            reversedNumber = 10 * reversedNumber + value % 10;
+            value /= 10;
+
+        }
+
+        return nr == reversedNumber;
+    }
+
 }
