d90ab0cd9412ddafe441ce4f57fa2edfdc42194f
Maíra Oliveira <mairasetecores@gmail.com>
2017-11-13 17:55:53 -0200

    creating exercise-8

---
+++ Week one/exercise-8/src/Process.java
@@ -0,0 +1,10 @@
+public class Process {
+
+    public void start() {
+
+        Service service = new Service();
+
+        System.out.println("The greatest product of thirteen adjacent digits is: " + service.theGreatestProductOfTheThirteenAdjacentDigits());
+
+    }
+}
