53f818e8a6f9c9dbf92fa9107e60e70b14752d32
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-11-15 13:39:33 -0200

    Adding tests to exercise-10

---
+++ Week one/exercise-10/src/test/java/Service/ServicePrimeTest.java
@@ -0,0 +1,37 @@
+package Service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServicePrimeTest {
+
+    private ServicePrime service;
+
+    @Before
+    public void setUp(){
+        service = new ServicePrime();
+    }
+
+    @Test
+    public void isPrimeTestWhenValueisPrimeShouldReturnTrue(){
+        Assert.assertTrue(service.isPrime(7));
+    }
+
+    @Test
+    public void isPrimeTestWhenValueisPrimeWithPointShouldReturnTrue(){
+        Assert.assertTrue(service.isPrime(7.0));
+    }
+
+    @Test
+    public void isPrimeTestWhenValueisNegativePrimeShouldReturnTrue(){
+        Assert.assertTrue(service.isPrime(-7));
+    }
+
+    @Test
+    public void isPrimeTestWhenValueisNotPrimeShouldReturnFalse(){
+        Assert.assertFalse(service.isPrime(4));
+    }
+
+
+}
