59eeff50cc28f80997b6377e65cd6edf1f3f3cf6
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-11-24 13:30:22 -0200

    Refactorin exercise 3

--- week two/exercicio-03/src/main/java/Runner.java
+++ week two/exercicio-03/src/main/java/Runner.java
@@ -1,9 +1,5 @@
 public class Runner {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        new Process().consultPerson();
-=======
         new Process().start();
->>>>>>> 57d025f478323f23021846726f2825a34527c051
     }
 }
--- week two/exercicio-03/src/main/java/Service/ServicePerson.java
+++ week two/exercicio-03/src/main/java/Service/ServicePerson.java
@@ -7,11 +7,13 @@ import java.util.ArrayList;
 public class ServicePerson {
     private  int age = 0;
     private double percentage = 0;
-    private int aux = 0;
+    private int quantityValidWoman = 0;
+    private ArrayList<Person> lista;
 
-    public void ConsultaPerson(){
 
-        ArrayList<Person> lista = new ArrayList<>();
+
+    public ServicePerson() {
+        lista = new ArrayList<>();
 
         lista.add(new Person("F", "louros", "azul", 20));
         lista.add(new Person("M", "pretos", "castanho", 31));
@@ -21,23 +23,34 @@ public class ServicePerson {
         lista.add(new Person("F", "louros", "verde", 35));
         lista.add(new Person("F", "louros", "verde", 27));
 
+    }
+
+    public void ConsultaPerson(){
 
         for(int i = 0; i < lista.size(); i++){
-            if(age < lista.get(i).getAge()) {
-                if (lista.get(i).getAge() < lista.get(i + 1).getAge()) {
-                    age = lista.get(i + 1).getAge();
-                }
-            }if(lista.get(i).getSex().contains("F") && lista.get(i).getHairColor().contains("louros") && lista.get(i).getEyesColor().contains("verde") && lista.get(i).getAge() >= 18 && lista.get(i).getAge() <= 35 ){
-                aux ++;
+            if(isTheOldest(i)) {
+                this.age = lista.get(i).getAge();
+            }
+
+            if(validateWoman(i)){
+                quantityValidWoman++;
             }
         }
+        System.out.printf("Pessoa Mais Velha " + age + " anos \n");
+
         System.out.println(calculatePersentage(lista) + "% das pessoas são do sexo feminino e de olhos verdes e cabelo loiro");
-        System.out.println("Pessoa Mais Velha tem " + age + " anos");
     }
 
     private double calculatePersentage(ArrayList<Person> lista){
-        percentage = (aux * 100)/lista.size();
+        percentage = (quantityValidWoman * 100)/lista.size();
         return percentage;
     }
 
+    private boolean isTheOldest(int i){
+        return age < lista.get(i).getAge();
+    }
+
+    private boolean validateWoman(int i){
+        return lista.get(i).getSex().contains("F") && lista.get(i).getHairColor().contains("louros") && lista.get(i).getEyesColor().contains("verde") && lista.get(i).getAge() >= 18 && lista.get(i).getAge() <= 35;
+    }
 }
