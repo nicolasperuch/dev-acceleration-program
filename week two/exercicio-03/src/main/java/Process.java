b29cec463e6acfa4c805ddc0f69ff8e74d896ce7
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-11-24 13:16:59 -0200

    Refactoring exercise 3

--- week two/exercicio-03/src/main/java/Process.java
+++ week two/exercicio-03/src/main/java/Process.java
@@ -1,9 +1,51 @@
 import Service.ServicePerson;
 
 public class Process {
+<<<<<<< HEAD
+    private int age = 0;
+
+    private ArrayList<Person> lista;
+
+    public Process() {
+        lista = new ArrayList<>();
+
+        lista.add(new Person("F", "louros", "azul", 20));
+        lista.add(new Person("M", "pretos", "castanho", 31));
+        lista.add(new Person("F", "castanhos", "verde", 30));
+        lista.add(new Person("F", "castanhos", "preto", 25));
+        lista.add(new Person("F", "louros", "verde", 18));
+        lista.add(new Person("F", "louros", "verde", 35));
+        lista.add(new Person("F", "louros", "verde", 27));
+
+    }
+
+    public void consultPerson(){
+
+        for(int i = 0; i < lista.size(); i++){
+            if(isTheOldest(i)) {
+                this.age = lista.get(i).getAge();
+            }
+
+            if(validateWoman(i)){
+                System.out.println("Mulher que satisfaca o exercicio 2: " + lista.get(i).getAge() + " anos");
+            }
+        }
+        System.out.printf("Pessoa Mais Velha " + age + " anos");
+    }
+
+
+    private boolean isTheOldest(int i){
+        return age < lista.get(i).getAge();
+    }
+
+    private boolean validateWoman(int i){
+        return lista.get(i).getSex().contains("F") && lista.get(i).getHairColor().contains("louros") && lista.get(i).getEyesColor().contains("verde") && lista.get(i).getAge() >= 18 && lista.get(i).getAge() <= 35;
+    }
+=======
    private ServicePerson servicePerson = new ServicePerson();
 
    public void start(){
        servicePerson.ConsultaPerson();
    }
+>>>>>>> 57d025f478323f23021846726f2825a34527c051
 }
