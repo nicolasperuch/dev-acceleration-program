85f57f01478ca18e6128d626a972be72e309b056
Adriel Domagalski <adriel.soares3@gmail.com>
2017-11-25 23:10:12 -0200

    correção exercicio 2

--- week two/exercise-02/src/main/java/Service/Service.java
+++ week two/exercise-02/src/main/java/Service/Service.java
@@ -1,11 +1,11 @@
 package Service;
 
 public class Service {
-    private final double PERCENTAGE_DISTRIBUTION = 28;
-    private final double TAX = 45;
+    private final double PERCENTAGE_DISTRIBUTION = 0.28;
+    private final double TAX = 0.45;
 
     public double calculateValueWhithPercen(double value) {
-        double result = ((value * PERCENTAGE_DISTRIBUTION)/100) + ((value * TAX)/100) + value ;
+        double result = (value * PERCENTAGE_DISTRIBUTION) + (value * TAX) + value ;
         return result;
     }
 }
