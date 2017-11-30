fba82081bce92e898a07ad62ace7b3adc210e5d5
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-11-30 19:23:43 -0200

    Adding tests

--- week three/refactor week one/exercicio-9/src/test/java/PythagoreanTripletTest.java
+++ week three/refactor week one/exercicio-9/src/test/java/PythagoreanTripletTest.java
@@ -1,3 +1,4 @@
+import business.PythagoreanTripletBusiness;
 import service.Service;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,15 +8,36 @@ import org.junit.Test;
  * Created by ilegra0267 on 17/11/17.
  */
 public class PythagoreanTripletTest {
-    private Service service;
+    private PythagoreanTripletBusiness pythagoreanTripletBusiness;
 
     @Before
     public void serUp(){
-        service = new Service();
+        pythagoreanTripletBusiness = new PythagoreanTripletBusiness();
     }
 
     @Test
-    public void TestServicePythagorean(){
-        Assert.assertEquals(service.Validete(1000), 31875000);
+    public void potentiationTest(){
+        int value = 10;
+
+        Assert.assertEquals(100, pythagoreanTripletBusiness.potentiation(value),0);
     }
+
+    @Test
+    public void sumOfAllValuesTest(){
+        int firstValue = 5;
+        int secondValue = 10;
+        int thirdValue = 11;
+
+        Assert.assertEquals(26, pythagoreanTripletBusiness.sumOfAllValues(firstValue, secondValue, thirdValue),0 );
+    }
+
+    @Test
+    public void calculatePythagoreanTripletValueTest(){
+        int firstValue = 5;
+        int secondValue = 10;
+        int thirdValue = 11;
+
+        Assert.assertEquals(550, pythagoreanTripletBusiness.calculatePythagoreanTripletValue(firstValue, secondValue, thirdValue),0 );
+    }
+
 }
